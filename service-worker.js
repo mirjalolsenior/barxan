1b129b572376dab65117b306bb5b91c5b1accd14
mirjalolsenior <amontagayev@gmail.com>
2025-08-06 22:53:41 +0500

    add

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('barxan-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './main.js',
+        './icon.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
